cf112780d27c974c27ae88be67852dba40d0c187
XNAT system <j.b.c.marsman@umcg.nl>
2018-03-06 13:18:38 +0100

    first commit

---
+++ pyxnat_scripts/first.py
@@ -0,0 +1,32 @@
+import os
+
+from pyxnat import Interface
+from xml.dom import minidom
+
+cnc = Interface(server='http://192.168.10.17:8080', user='python', password='cnc_python2018', cachedir='/tmp')
+
+projects = cnc.select.projects().get()
+
+
+subjects = cnc.select.projects('*').subjects()
+
+for s in subjects:
+
+    experiments = s.experiments()
+    for e in experiments:
+        dicoms = e.scans()
+        for d in dicoms:
+            str = d.get()
+            xml = minidom.parseString(str)
+
+            elm = xml.getElementsByTagName("xnat:MRScan")
+
+            #scantype = elm[0].attributes["type"].value
+
+            #if scantype == 'MPRAGE':
+            os.mkdir('/data/xnat/tmp/cnc_converter')
+            dicoms.download('/data/xnat/tmp/cnc_converter/', type='ALL', extract=False)
+
+        
+    
+    
